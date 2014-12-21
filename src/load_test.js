ac059e9afdb985dca6d9e48f43ddf5a6877446b9
Chris Geihsler <chris@geihsler.net>
2014-12-21 08:54:55 -0500

    Moved most of script into src and added student and teacher.

---
+++ src/load_test.js
@@ -0,0 +1,48 @@
+module.exports = function(options) {
+  var url = options.url;
+
+  var faye    = require('faye');
+  var teacher = new faye.Client(url);
+  var student = new faye.Client(url);
+
+  teacher.bind('transport:down', function() {
+    console.log('Disconnected.');
+  });
+
+  teacher.bind('transport:up', function() {
+    console.log('Connected.');
+  });
+
+  //teacher
+  //connect
+  //wait for students to pop in the queue
+  //grab a student
+  //go into a private channel
+  //send messages
+  //leave
+
+  teacher.publish('/presence/connect', {
+      userId: 1,
+      role:   'teacher'
+  });
+
+  //student
+  //connect
+  //wait for teacher to grab student
+  //go into a private channel
+  //send messages
+  //leave
+
+  student.publish('/presence/connect', {
+      userId: 100,
+      role:   'student'
+  });
+
+  teacher.subscribe('/presence/status', function(data) {
+    console.log('teacher', data);
+  });
+
+  student.subscribe('/presence/status', function(data) {
+    console.log('student', data);
+  });
+}
