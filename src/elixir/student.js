a3aa78c0af94144a7b274e403b5938af38a4d3cc
Chris Geihsler <chris@geihsler.net>
2015-01-14 19:39:41 -0500

    All students are in one channel.

--- src/elixir/student.js
+++ src/elixir/student.js
@@ -7,32 +7,34 @@ module.exports = function(options) {
     var socket  = new Phoenix.Socket(url);
     var student = { userId: id, role: 'student' };
 
-    socket.join("presence:student:" + id, student, function(channel) {
+    socket.join("presence:students", student, function(channel) {
       channel.on("new:chat", function(chat) {
-        socket.join("chats:" + chat.id, student, function(chatChannel) {
-          console.log('Student ' + id + ' is starting new chat.');
-          var messageCount = 0;
+        if(chat.student_id == id) {
+          socket.join("chats:" + chat.id, student, function(chatChannel) {
+            console.log('Student ' + id + ' is starting new chat.');
+            var messageCount = 0;
 
-          chatChannel.on("chat:terminated", function(data) {
-            console.log('Student ' + id + ' got disconnect message.');
-            channel.leave();
-            socket.close();
-          });
-
-          chatChannel.on("student:receive", function(data) {
-            messageCount++;
-
-            if(messageCount == 1) {
-              console.log('Student ' + id + ' got first message.');
-            }
-
-            chatChannel.send("student:send", {
-              message: "Message #" + messageCount + " from student: " + id
+            chatChannel.on("chat:terminated", function(data) {
+              console.log('Student ' + id + ' got disconnect message.');
+              channel.leave();
+              socket.close();
             });
-          });
 
-          chatChannel.send("student:joined", {});
-        });
+            chatChannel.on("student:receive", function(data) {
+              messageCount++;
+
+              if(messageCount == 1) {
+                console.log('Student ' + id + ' got first message.');
+              }
+
+              chatChannel.send("student:send", {
+                message: "Message #" + messageCount + " from student: " + id
+              });
+            });
+
+            chatChannel.send("student:joined", {});
+          });
+        }
       });
 
       channel.send("student:ready", {userId: id});
