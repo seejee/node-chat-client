6b2b3d463de63615a5a310c6caed5db9fe487759
Chris Geihsler <chris@geihsler.net>
2015-01-14 22:50:00 -0500

    All in the same channel, but subscribing to a private event.

--- src/elixir/student.js
+++ src/elixir/student.js
@@ -8,33 +8,31 @@ module.exports = function(options) {
     var student = { userId: id, role: 'student' };
 
     socket.join("presence:students", student, function(channel) {
-      channel.on("new:chat", function(chat) {
-        if(chat.student_id == id) {
-          socket.join("chats:" + chat.id, student, function(chatChannel) {
-            console.log('Student ' + id + ' is starting new chat.');
-            var messageCount = 0;
+      channel.on("new:chat:" + id, function(chat) {
+        socket.join("chats:" + chat.id, student, function(chatChannel) {
+          console.log('Student ' + id + ' is starting new chat.');
+          var messageCount = 0;
 
-            chatChannel.on("chat:terminated", function(data) {
-              console.log('Student ' + id + ' got disconnect message.');
-              channel.leave();
-              socket.close();
-            });
-
-            chatChannel.on("student:receive", function(data) {
-              messageCount++;
-
-              if(messageCount == 1) {
-                console.log('Student ' + id + ' got first message.');
-              }
-
-              chatChannel.send("student:send", {
-                message: "Message #" + messageCount + " from student: " + id
-              });
-            });
-
-            chatChannel.send("student:joined", {});
+          chatChannel.on("chat:terminated", function(data) {
+            console.log('Student ' + id + ' got disconnect message.');
+            channel.leave();
+            socket.close();
           });
-        }
+
+          chatChannel.on("student:receive", function(data) {
+            messageCount++;
+
+            if(messageCount == 1) {
+              console.log('Student ' + id + ' got first message.');
+            }
+
+            chatChannel.send("student:send", {
+                message: "Message #" + messageCount + " from student: " + id
+            });
+          });
+
+          chatChannel.send("student:joined", {});
+        });
       });
 
       channel.send("student:ready", {userId: id});
